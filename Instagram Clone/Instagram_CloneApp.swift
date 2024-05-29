26bf24ebf6470430d80639ee626522fcc0c34910
Gourob Mazumder <gourob.mazumder@enosisbd.com>
2024-05-29 23:24:35 +0600

    Initial Commit

---
+++ Instagram Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram Clone
+//
+//  Created by Gourob Mazumder on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
